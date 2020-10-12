13b97bc550d0029072f7c4dda1b82f5375c4ce8e
Michael Zaslavsky <mikezasl@gmail.com>
2020-10-12 00:52:05 -0500

    pushing

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,38 +1,11 @@
 USE employeesDB;
 
 INSERT INTO department (name)
-VALUES ("Sales");
+VALUES ("Twitch");
 INSERT INTO department (name)
-VALUES ("Engineering");
+VALUES ("Gaming");
 INSERT INTO department (name)
-VALUES ("Finance");
+VALUES ("Mods");
 INSERT INTO department (name)
-VALUES ("Legal");
+VALUES ("Just Chatting");
 
-INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Lead", 100000, 1);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Lead Engineer", 150000, 2);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Software Engineer", 120000, 2);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Accountant", 125000, 3);
-INSERT INTO role (title, salary, department_id)
-VALUES ("Legal Team Lead", 250000, 4);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, 3);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Mike", "Chan", 2, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Ashley", "Rodriguez", 3, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Kevin", "Tupik", 4, 3);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Malia", "Brown", 5, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Sarah", "Lourd", 2, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tom", "Allen", 4, 7);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Christian", "Eckenrode", 1, 2);
